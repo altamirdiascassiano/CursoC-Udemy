9e8529445c68218162486996206e0b102ab64092
Altamir Cassiano <altamir.cassiano@energisa.com.br>
2021-08-15 21:45:41 -0300

    Finalizando Enum

---
+++ CursoC++/EnumFunctorsAritmeticaComPonteiro/classes-enum.cpp
@@ -0,0 +1,32 @@
+/* Somente um arquivo pode ter o MAIN
+#include <iostream>
+#include <string>
+
+enum class Cores {
+	color_blue,
+	color_purple
+};
+
+enum class Frutas {
+	laranja,
+	pessego,
+	banana,
+	uva
+};
+
+static const char* enum_str_frutas[] = { "laranja", "pessego", "banana", "uva" };
+
+int main(int argc, char** argv) {
+	Cores cores{ Cores::color_blue };
+	Frutas frutas{ Frutas::laranja };
+
+
+	std::cout << "O número de cores é: " << static_cast<int>(cores) << '\n';
+	std::cout << "Fruta: " << enum_str_frutas[static_cast<int>(Frutas::uva)] << '\n';
+
+
+
+	return 0;
+}
+
+*/
---
+++ CursoC++/EnumFunctorsAritmeticaComPonteiro/functor.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+struct Estrutura {
+	//https://www.udemy.com/course/curso-de-cpp-moderno-avancado/learn/lecture/24597792#overview
+};
+
+int main(int argc, char** argv) {
+	std::cout << "Olá, Mundo!" << '\n';
+	return 0;
+}
